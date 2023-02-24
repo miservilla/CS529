2e3e9312c509a50c1c602e2d76220c285c5ca852
Michael Servilla <miservilla@gmail.com>
2023-02-23 22:55:55 -0700

    main loads sparse from disk.

--- Project_2/main.py
+++ Project_2/main.py
@@ -6,21 +6,9 @@ from numpy import genfromtxt
 from numpy import asarray
 from numpy import savetxt
 
+train_sparse = sparse.load_npz('/home/sin_nombre/CS529/Project_2/csr_train.csv.npz')
 
-# df = pd.read_csv('/home/michaelservilla/CS529/Project_2/training.csv', sep=',')
-# df = df.drop(df.columns[0], axis=1)
+train = sparse.csr_matrix(train_sparse)
 
-train = genfromtxt('/home/michaelservilla/CS529/Project_2/training.csv', delimiter=',')
-train = np.delete(train,0,1)
 print(train)
-print(train[:, 0])
-print(train.data.shape)
-# x = StandardScaler().fit_transform(train.data)
-# print(x)
-train_sparse = csr_matrix(train, dtype=np.int8)
-print(train_sparse)
-print(train_sparse.shape)
-
-
-sparse.save_npz('/home/michaelservilla/CS529/Project_2/csr_train.csv.npz', train_sparse)
-# train_sparse = sparse.load_npz('/home/michaelservilla/CS529/Project_2/csr_train.csv.npz')
+print(train.data)
