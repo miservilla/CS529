f982fd29a180cf0b9fa1d633368c4ed51df53ef0
lservilla <lservilla@unm.edu>
2023-03-04 00:25:33 -0700

    cleaned up main

--- Project_2/main.py
+++ Project_2/main.py
@@ -25,6 +25,8 @@ lh_arr = likelihood (2d array with newsgroups as rows and words as columns, last
 column is prior)
 
 TODO
+    ********* See todo lower in document for what to work on next, it's in the
+              location that it should go inside the document                   **************
 
 
 
@@ -41,6 +43,20 @@ from numpy import savetxt
 from numpy import loadtxt
 import math
 
+
+# Loading training data 
+train_sparse = sparse.load_npz(
+    'Project_2/csr_train.csv.npz')
+
+
+# Adjust these global values for specific training set
+train = sparse.csr_matrix(train_sparse) # Traing data as sparce matrix
+unique_targets = 20 # Number of possible targets (number of news groups)
+total_docs = train.shape[0] # Number of documents in trianing set
+v_total = 61188 # Number of unique words in training set
+column_count = train.shape[1] # Number of columns prior to removing unneeded index and target columns
+
+
 def mle(yk_docs_cnt, total_docs):
     return(yk_docs_cnt / total_docs) # prior
 
@@ -50,70 +66,77 @@ def alpha(v_total):
 def map(x_i, yk_words, v_total):
     return ((x_i + (alpha(v_total) - 1)) / (yk_words + ((alpha(v_total)-1) * v_total))) # likelihood
 
-lh = [[0 for x in range(61189)] for y in range(21)]
+def build_MAP_MLE(lh, beta): #Uses global values
+    df = pd.DataFrame(columns=['index', 'newsgroups'])
 
-train_sparse = sparse.load_npz(
-    'Project_2/csr_train.csv.npz')
+    for i in range(total_docs):
+        df.loc[len(df.index)] = [i, train[i, 61189]] # df starts at index 0
 
-train = sparse.csr_matrix(train_sparse)
-total_docs = train.shape[0]
+    dataframes = {} # Dictionary of dataframes with newsgroups indexes 
+    for i in range(1, unique_targets+1):
+        dataframes["df_{0}".format(i)] = df[df['newsgroups'] == i]
 
-print(train.shape)
-print(train.shape[0])
-print(train.shape[1])
+    wrd_per_NG = csr_matrix((unique_targets,v_total+1), dtype=int)
 
-df = pd.DataFrame(columns=['index', 'newsgroups'])
+    sparse_newsgroups = {} # Dictionary of sparse matrix for each newsgroup
+    count = 1
 
-for i in range(9600):
-    df.loc[len(df.index)] = [i, train[i, 61189]] # df starts at index 0
+    for dataframe in dataframes:
+        print()
+        print("News Group:" + str(dataframes[dataframe].iloc[0]['newsgroups']))
+        print("Size: " + str(len(dataframes[dataframe])))
+        yk_docs_cnt = len(dataframes[dataframe])
+        index = str(dataframes[dataframe].iloc[0]['newsgroups'])
 
-dataframes = {} # Dictionary of dataframes with newsgroups indexes 
-for i in range(1, 21):
-    dataframes["df_{0}".format(i)] = df[df['newsgroups'] == i]
+        # Summing each row associated with current news group
+        sparse_newsgroups["{0}".format(index)] = csr_matrix((1,column_count), dtype=int)
+        for row in dataframes[dataframe]['index']:
+            sparse_newsgroups[index] = train[row].toarray() + sparse_newsgroups[index]
 
-wrd_per_NG = csr_matrix((20,61189), dtype=int)
+        # Deleting 1st and last column as they are index and news groups (not words)
+        sparse_newsgroups[index] = np.delete(sparse_newsgroups[index], 0)
+        sparse_newsgroups[index] = np.delete(sparse_newsgroups[index], v_total)
+        print(sparse_newsgroups[index])
+        
+        yk_words = np.sum(sparse_newsgroups[index]) # Sum of all words in current news group
+        
+        for i in range(v_total):
+            x_i = sparse_newsgroups[index][0, i]
+            lh[count][i] = map(x_i, yk_words, beta)
 
-sparse_newsgroups = {} # Dictionary of sparse matrix for each newsgroup
-v_total = 61188
-count = 1
+        lh[count][61188] = mle(yk_docs_cnt, total_docs)
+        # print("Test MAP: " + str(lh[count][0]) + " with x_i of " + str(sparse_newsgroups[index][0, 0]))
+        # print("word count of " + str(yk_words) + " and v_total of " + str(v_total))
+        # print("MLE: " + str(lh[count][61188]))
 
-for dataframe in dataframes:
-    print()
-    print("News Group:" + str(dataframes[dataframe].iloc[0]['newsgroups']))
-    print("Size: " + str(len(dataframes[dataframe])))
-    yk_docs_cnt = len(dataframes[dataframe])
-    index = str(dataframes[dataframe].iloc[0]['newsgroups'])
+    
+        count += 1
+    return lh
 
-    # Summing each row associated with current news group
-    sparse_newsgroups["{0}".format(index)] = csr_matrix((1,61190), dtype=int)
-    for row in dataframes[dataframe]['index']:
-        sparse_newsgroups[index] = train[row].toarray() + sparse_newsgroups[index]
 
-    # Deleting 1st and last column as they are index and news groups (not words)
-    sparse_newsgroups[index] = np.delete(sparse_newsgroups[index], 0)
-    sparse_newsgroups[index] = np.delete(sparse_newsgroups[index], 61188)
-    print(sparse_newsgroups[index][0,0:25])
+# Getting MAP and MLE values for beta = v_total 
+lh = build_MAP_MLE([[0 for x in range(v_total+1)] for y in range(unique_targets+1)], beta=v_total) 
 
-    yk_words = np.sum(sparse_newsgroups[index]) # Sum of all words in current news group
-    print(yk_words)
-
-    for i in range(v_total):
-        x_i = sparse_newsgroups[index][0, i]
-        lh[count][i] = map(x_i, yk_words, v_total)
-
-    lh[count][61188] = mle(yk_docs_cnt, total_docs)
-    # print("Test MAP: " + str(lh[count][0]) + " with x_i of " + str(sparse_newsgroups[index][0, 0]))
-    # print("word count of " + str(yk_words) + " and v_total of " + str(v_total))
-    # print("MLE: " + str(lh[count][61188]))
-
- 
-    count += 1
-
-print()
+# Turning lh into an array and then saving it to a file 
 lh_np = np.asarray(lh)
 lh_np = np.delete(lh_np, 0, 0) # delete row 0, all zero's
 savetxt('Project_2/lh_np.csv', lh_np, delimiter=',')
 
+
+'''
+TODO here 
+
+    Need to create a loop to make and *save MAP_MLE array for beta values 0.00001 to 1
+    not sure if we actually want to be saving that many files or convert code in test
+    file to a function that can be called in main....See question 2 in the PDF for Project
+    2 for more clarification on what I am talking about
+
+
+'''
+
+
+
+# *************************************** Not sure what you're doing here????? *****************************************************
 test_sparse = sparse.load_npz('Project_2/csr_test.csv.npz')
 test = sparse.csr_matrix(test_sparse)
 test_np = csr_matrix.toarray(test)
@@ -122,6 +145,7 @@ test_np = np.delete(test_np, 0, 1) # delete 1st column (index)
 product = [[0 for x in range(20)] for y in range(2400)]
 product = np.array(product)
 
+print()
 print(lh_np.shape)
 print(test_np.shape)
 print(product.shape)       
