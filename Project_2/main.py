383f21019e6f78b1ae9eb3316a5c2fd1378e3b23
lservilla <lservilla@unm.edu>
2023-03-03 23:38:11 -0700

    It gets 0.855 now.

--- Project_2/main.py
+++ Project_2/main.py
@@ -42,13 +42,13 @@ from numpy import loadtxt
 import math
 
 def mle(yk_docs_cnt, total_docs):
-    return(yk_docs_cnt / total_docs)# prior
+    return(yk_docs_cnt / total_docs) # prior
 
 def alpha(v_total):
-    return(1 + (1 / v_total))# alpha
+    return(1 + (1 / v_total)) # alpha
 
-def map(x_i, yk_words, v_total):# likelihood
-    return ((x_i + (alpha(v_total) - 1)) / (yk_words + (alpha(v_total) * v_total)))
+def map(x_i, yk_words, v_total):
+    return ((x_i + (alpha(v_total) - 1)) / (yk_words + ((alpha(v_total)-1) * v_total))) # likelihood
 
 lh = [[0 for x in range(61189)] for y in range(21)]
 
@@ -56,6 +56,7 @@ train_sparse = sparse.load_npz(
     'Project_2/csr_train.csv.npz')
 
 train = sparse.csr_matrix(train_sparse)
+total_docs = train.shape[0]
 
 print(train.shape)
 print(train.shape[0])
@@ -71,37 +72,42 @@ for i in range(1, 21):
     dataframes["df_{0}".format(i)] = df[df['newsgroups'] == i]
 
 wrd_per_NG = csr_matrix((20,61189), dtype=int)
-wrd_count = 61188
 
 sparse_newsgroups = {} # Dictionary of sparse matrix for each newsgroup
 v_total = 61188
 count = 1
 
 for dataframe in dataframes:
+    print()
     print("News Group:" + str(dataframes[dataframe].iloc[0]['newsgroups']))
     print("Size: " + str(len(dataframes[dataframe])))
     yk_docs_cnt = len(dataframes[dataframe])
     index = str(dataframes[dataframe].iloc[0]['newsgroups'])
+
+    # Summing each row associated with current news group
     sparse_newsgroups["{0}".format(index)] = csr_matrix((1,61190), dtype=int)
     for row in dataframes[dataframe]['index']:
-        sparse_newsgroups[index] = train[row].toarray() + sparse_newsgroups[index].copy()
+        sparse_newsgroups[index] = train[row].toarray() + sparse_newsgroups[index]
 
-    print(sparse_newsgroups[index][0,1])
+    # Deleting 1st and last column as they are index and news groups (not words)
     sparse_newsgroups[index] = np.delete(sparse_newsgroups[index], 0)
     sparse_newsgroups[index] = np.delete(sparse_newsgroups[index], 61188)
-    print(sparse_newsgroups[index])
-    print(np.sum(sparse_newsgroups[index]))
-    total_docs = np.sum(sparse_newsgroups[index])
+    print(sparse_newsgroups[index][0,0:25])
 
-    yk_words = np.sum(sparse_newsgroups[index])
-    for i in range(61188):
+    yk_words = np.sum(sparse_newsgroups[index]) # Sum of all words in current news group
+    print(yk_words)
+
+    for i in range(v_total):
         x_i = sparse_newsgroups[index][0, i]
         lh[count][i] = map(x_i, yk_words, v_total)
 
     lh[count][61188] = mle(yk_docs_cnt, total_docs)
+    # print("Test MAP: " + str(lh[count][0]) + " with x_i of " + str(sparse_newsgroups[index][0, 0]))
+    # print("word count of " + str(yk_words) + " and v_total of " + str(v_total))
+    # print("MLE: " + str(lh[count][61188]))
 
+ 
     count += 1
-
 
 print()
 lh_np = np.asarray(lh)
