eeb2a446dc3f8cddf3a823e81b6b49c4e482542f
lservilla <lservilla@unm.edu>
2023-03-18 12:50:42 -0600

    Saving so I can revert if I mess something up

--- Project_2/lr_main.py
+++ Project_2/lr_main.py
@@ -3,125 +3,139 @@ from scipy.sparse import csr_matrix, lil_matrix
 from scipy import sparse
 import numpy as np
 
-'''
-total_docs = number of examples (m in PDF)
-unique_targets = number of classes (in this context number of news groups)
-v_total = number of attributes -> unique words in training set (n in PDF)
-aprox_prediction = exp(WX^T) used in place of P(Y|W,X)
-delta = 1 or 0 depending on if Y^l = yj or Y^l !+ yj
 
-'''
+''' Global Variables Start: Adjust these global values for specific training set '''
 
-''' Global Variables Start '''
 # Loading training data 
 train_sparse = sparse.load_npz(
-    '/home/michaelservilla/CS529/Project_2/csr_train.csv_sm.npz')
+    'Project_2/csr_train.csv_sm.npz')
 
-# Adjust these global values for specific training set
-train = sparse.csr_matrix(train_sparse) # Traing data as sparse matrix
-print(train.shape)
+# Traing data as sparse matrix
+train = sparse.csr_matrix(train_sparse) 
 
-unique_targets = train[np.argmax(train[:, -1]), -1] # Number of possible targets
-#(number of news groups) based on last column values
-total_docs = train.shape[0] # Number of documents in trianing set
-v_total = train.shape[1] - 2  # Number of unique words in training set, need to 
-#drop index and class columns, so - 2
-column_count = train.shape[1] # Number of columns prior to removing unneeded 
-#index and target columns
-Ys = train[:,column_count-1].toarray() # A m by 1 vector of true classifications
-Ys -= 1
+# Number of possible targets (number of news groups) based on last column values
+unique_targets = train[np.argmax(train[:, -1]), -1] 
+
+# Number of documents in trianing set
+total_docs = train.shape[0] 
+
+# Number of unique words in training set, need to drop index and class columns, so - 2
+v_total = train.shape[1] - 2  
+
+# Number of columns prior to removing unneeded index and target columns
+column_count = train.shape[1]
+
+# A v_total by 1 vector of true classifications
+y = train[:,column_count-1].toarray() 
+# print(y)
+
+# Learning rate used to update the weights
 learning = 0.01
+
+# Penalty applied to the weights while updating them
 penalty = 0.01
-max_iterations = 100000
 
-# Getting matrix of Xs 
-Xs = train.copy()
-Xs[:,0] = np.ones(total_docs)
-Xs = Xs[:, :-1]
+# Number of times the weights get updated
+max_iterations = 10000
 
-# Getting matrix of delta values
-def get_delta():
-    delta_matrix = np.zeros(shape=(unique_targets, total_docs))
-    for i in range(delta_matrix.shape[0]):
-        for j in range(delta_matrix.shape[1]):
-            if i == Ys[j,0]:
-                delta_matrix[i,j] = 1
+# Setting matrix for X where the first column is all 1's to deal with W0
+x = train.copy()
+x[:,0] = np.ones(total_docs)
+x = x[:, :-1]
+# print(x.toarray())
 
-    # print(delta_matrix)
-    return delta_matrix
-delta = get_delta()
+# Setting matrix for delta where if Yj = Yi the value is 1, otherwie 0
+delta = np.zeros(shape=(unique_targets, total_docs))
+for i in range(delta.shape[0]):
+    for j in range(delta.shape[1]):
+        if i == y[j,0]-1:
+            delta[i,j] = 1
+# print(delta)
+
 ''' Global Variables End '''
 
+# Initializing weights with 0's
+def set_weights(x_length) -> np.ndarray: 
+    weights = np.zeros(shape=(unique_targets,v_total+1))
+    return weights
 
-def set_weights(Xs_length): # Initializing weights with random floats
-    return np.random.rand(1, Xs_length)
-
-# def update_weights(current_weights, learning, penalty, aprox_prediction):    
-#     return current_weights + (  learning * (((delta - aprox_prediction) * Xs).sum(axis=0) - (penalty * current_weights)))
-
-def get_approx_prediction(weights): # Returns the P(Y|W,X) from in the PDF
-    print(np.exp(np.float128((weights*np.transpose(Xs)).max())))
-    predict = np.exp(np.float128(weights*np.transpose(Xs)))
-    predict[unique_targets-1,:] = 1
-    # print(predict)
-    pt = np.transpose(predict)
-    predict = np.transpose(pt/pt.sum(axis=1)[:,None])
-    # print(predict)
-    return predict
-
-def get_likelihood(Yl, weights, Xljs):
-    # print(Yl)
-    # print(weights)
-    # print(Xljs[0])
-    # print(np.dot(weights,Xljs[0]))
-    return int(Yl)* np.dot(weights,Xljs[0])- np.log(1 + np.exp(np.float128(np.dot(weights,Xljs[0]))))# Conditional likelihood equation from PDF
+def print_prediction(prediction):
+    print("Prediction: ------------------------------------------------")
+    rows = prediction.shape[0]
+    cols = prediction.shape[1]
+    for i in range(rows):
+        for j in range(cols):
+            print(str(prediction[i][j])[:7], end=" ")
+        print()
 
 
-def minimize_likelihood(last_likelihood, weights_matrix):
-    # print()
-    # print("****************************************************************")
+# Returns the predicted likelihood for each example that it is in a particular class -> P(Y|W,X) 
+def calculate_prediction(weights) -> np.ndarray: 
+    prediction = np.exp(np.float128(weights*np.transpose(x)))
+    prediction[unique_targets-1,:] = 1
+    pt = np.transpose(prediction)
+    prediction = np.transpose(pt/pt.sum(axis=1)[:,None])
 
-    weights_matrix2 = np.zeros(shape=(unique_targets,v_total+1))
+    print_prediction(prediction)
 
-    predict = get_approx_prediction(weights_matrix)
-    # print(predict)
+    return prediction
 
-    # for i in range(unique_targets): # Updating weights for each class
-        # print()
-        # print("Class: " + str(i+1))
-        # print(last_weights[cur_class])
-        # weights_matrix2[i] = weights_matrix[i] + (  learning * (((delta - predict) * Xs).sum(axis=0) - (penalty * weights_matrix[i])))
-
-    weights_matrix2 = weights_matrix + (  learning * (((delta - predict) * Xs) - (penalty * weights_matrix)))
-
-
-        # print(weights_matrix)
-        # print(weights_matrix2)
-
+# Returns the conditional likelihood value for the input weights 
+def calculate_likelihood(weights):
     likelihood = 0
 
     for j in range(total_docs):
-        target = Ys[j,0]
-        likelihood = likelihood + get_likelihood(target ,weights_matrix2[target,:], Xs[j,:].toarray())
+        # Target value for current example
+        target = y[j]-1
+        # print(target)
+        # Vector of X values for current example
+        xj = x[j,:].toarray()
+        # print(xj[0])
+        # vector of W (weight) values for current example's target
+        w = weights[target,:]
+        # print(w[0])
+        # Dot product of w and x
+        dot = np.dot(w[0],xj[0])
+        print(dot)
+        # Updating likelihood
+        likelihood = likelihood + ((y[j]* dot)- np.log(1 + np.exp(np.float128(dot))))
 
-    print()
-    print("Likelihood: " + str(likelihood))
+    return likelihood
 
-    if last_likelihood < likelihood:
-        return likelihood, weights_matrix2
+
+def maximize_likelihood(last_likelihood, last_weights):
+
+    new_weights = np.zeros(shape=(unique_targets,v_total+1))
+
+    prediction = calculate_prediction(last_weights)
+
+    new_weights = last_weights + (  learning * (((delta - prediction) * x) - (penalty * last_weights)))
+
+    new_likelihood = calculate_likelihood(new_weights)
+
+    if last_likelihood < new_likelihood:
+        print()
+        print("Likelihood: " + str(new_likelihood))
+        return new_likelihood, new_weights
     else:
-        return last_likelihood, weights_matrix
+        print()
+        print("Likelihood: " + str(last_likelihood))
+        return last_likelihood, last_weights
+    # return new_likelihood, new_weights
 
     
+# Returns matrix of weights that maximize the conditional likelihood function
+def find_weights(): 
 
-def build_weights(): # Returns array of maximizing weights (w)
+    ''' Dataframe dictionary not needed in final code, only used to see what class each example is in '''
 
     df = pd.DataFrame(columns=['index', 'class_id'])
 
     for i in range(total_docs):
-        df.loc[len(df.index)] = [i, train[i, column_count - 1]] # df starts at index 0
-
-    dataframes = {} # Dictionary of dataframes with newsgroups indexes 
+        df.loc[len(df.index)] = [i, train[i, column_count - 1]] 
+    
+    # Dictionary of dataframes with newsgroups indexes
+    dataframes = {}  
     for i in range(1, unique_targets+1):
         dataframes["df_{0}".format(i)] = df[df['class_id'] == i]
 
@@ -131,41 +145,31 @@ def build_weights(): # Returns array of maximizing weights (w)
         print("Size: " + str(len(dataframes[dataframe])))
         print(dataframes[dataframe])
 
+    ''' End of code used for dataframe dictionary '''
 
-    weights_matrix = np.zeros(shape=(unique_targets,v_total+1))
 
-    for i in range(unique_targets): # Creating matrix of weights
-        weights_matrix[i] = set_weights(v_total+1)
+    weights = set_weights(v_total+1)
     
-    likelihood = 0
-
-    for j in range(total_docs):
-        target = Ys[j,0]
-        likelihood = likelihood + get_likelihood(target,weights_matrix[target,:], Xs[j,:].toarray())
-
+    likelihood = calculate_likelihood(weights)
+    
     print()
     print("Likelihood: " + str(likelihood))
 
-    last_likelihood = likelihood-1
-    current_likelihood = likelihood
-    iter = 0
-
-    while last_likelihood < current_likelihood and iter < max_iterations:
-        last_likelihood = current_likelihood.copy()
-        current_likelihood, weights_matrix = minimize_likelihood(last_likelihood, weights_matrix)
+    for iter in range(max_iterations):
+        likelihood, weights = maximize_likelihood(likelihood, weights)
         iter += 1
 
-    return weights_matrix
+
+    return weights
 
 
       
 
-weights = build_weights()
+weights = find_weights()
+final_predictions = calculate_prediction(weights)
 
-# print(weights)
-get_approx_prediction(weights)# This prints out the final predictions where rows are the classes (news goups) and  columns are 
-# the training examples (documents used in training), so each value shown is the probability that the document (column) is from news group (row)
-# for the small set it prints as expected for the final weights however there is an overflow issue for the large database
-# so we might have to normalize the large data set in some way in order to bring down the size of the values
-
-# Nevermind there seems to be weirdness going on with the weights
+print("****************************************************************")
+print(weights)
+print()
+print_prediction(final_predictions)
+print("****************************************************************")
