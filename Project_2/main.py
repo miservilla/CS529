e8a203501da5f20d4bb61750b2646b8c72362134
lservilla <lservilla@unm.edu>
2023-03-22 22:50:06 -0600

    I tried my best but my computer isn't good enough

--- Project_2/main.py
+++ Project_2/main.py
@@ -24,12 +24,6 @@ yk_words = total number of words in yk (newsgroup)
 lh_arr = likelihood (2d array with newsgroups as rows and words as columns, last
 column is prior)
 
-TODO
-    ********* See todo lower in document for what to work on next, it's in the
-              location that it should go inside the document                   **************
-
-
-
 '''
 
 
@@ -44,33 +38,55 @@ from numpy import loadtxt
 import math
 
 
+'''
+Adjust these global variables for specific training set:
+'''
 # Loading training data 
 train_sparse = sparse.load_npz(
     'Project_2/csr_train.csv_lg.npz')
 
-
-# Adjust these global values for specific training set
 train = sparse.csr_matrix(train_sparse) # Traing data as sparce matrix
-print(train.shape)
+# print(train.shape)
 
 unique_targets = train[np.argmax(train[:, -1]), -1] # Number of possible targets
 #(number of news groups) based on last column values
+
 total_docs = train.shape[0] # Number of documents in trianing set
+
 v_total = train.shape[1] - 2  # Number of unique words in training set, need to 
 #drop index and class columns, so - 2
+
 column_count = train.shape[1] # Number of columns prior to removing unneeded 
 #index and target columns
 
+run_loop = False # Set to false so that only a single beta value run is completed 
+'''
+End of global variables  
+'''
 
+'''
+mle returns the prior for use in Naive Bayes
+'''
 def mle(yk_docs_cnt, total_docs):
     return(yk_docs_cnt / total_docs) # prior
 
-def alpha(v_total):
-    return(1 + (1 / v_total)) # alpha
+'''
+alpha returns the alpha value used by MAP 
+beta is structured as 1/'beta' due to the original beta value being 1/|V| 
+'''
+def alpha(beta):
+    return(1 + (1 / beta)) # alpha
 
+'''
+map returns the MAP estimate for use in Naive Bayes
+'''
 def map(x_i, yk_words, v_total, beta):
     return ((x_i + (alpha(beta) - 1)) / (yk_words + ((alpha(beta)-1) * v_total))) # likelihood
 
+'''
+build_MAP_MLE returns a matrix of MAP and MLE estimates for each class, the first 0 to v_total-1 columns
+contain the MAP estimate for each attribute and the final column contains the MLE estimate
+'''
 def build_MAP_MLE(lh, beta): #Uses global values
     df = pd.DataFrame(columns=['index', 'class_id'])
 
@@ -87,8 +103,8 @@ def build_MAP_MLE(lh, beta): #Uses global values
     count = 1
 
     for dataframe in dataframes:
-        # print()
-        # print("Class:" + str(dataframes[dataframe].iloc[0]['class_id']))
+        print()
+        print("Class:" + str(dataframes[dataframe].iloc[0]['class_id'])) # Showing progress 
         # print("Size: " + str(len(dataframes[dataframe])))
         yk_docs_cnt = len(dataframes[dataframe])
         index = str(dataframes[dataframe].iloc[0]['class_id'])
@@ -105,10 +121,12 @@ def build_MAP_MLE(lh, beta): #Uses global values
         
         yk_words = np.sum(sparse_classes[index]) # Sum of all words in current news group
         
+        # Finding the MAP values for each attribute for the current class
         for i in range(v_total):
             x_i = sparse_classes[index][0, i]
             lh[count][i] = map(x_i, yk_words, v_total, beta)
 
+        # Finding the MLE value for the current class
         lh[count][v_total] = mle(yk_docs_cnt, total_docs)
         # print("Test MAP: " + str(lh[count][0]) + " with x_i of " + str(sparse_newsgroups[index][0, 0]))
         # print("word count of " + str(yk_words) + " and v_total of " + str(v_total))
@@ -119,38 +137,34 @@ def build_MAP_MLE(lh, beta): #Uses global values
     return lh
 
 
-# Getting MAP and MLE values for beta = v_total 
-lh = build_MAP_MLE([[0 for x in range(v_total+1)] for y in range(unique_targets+1)], beta=v_total) 
-
-# Turning lh into an array and then saving it to a file (np ~ numpy)
-lh_np = np.asarray(lh)
-lh_np = np.delete(lh_np, 0, 0) # delete row 0, all zero's
-savetxt('Project_2/lh_np.csv', lh_np, delimiter=',')
-
-
 '''
-TODO here 
-
-    Need to create a loop to make and *save MAP_MLE array for beta values 0.00001 to 1
-    not sure if we actually want to be saving that many files or convert code in test
-    file to a function that can be called in main....See question 2 in the PDF for Project
-    2 for more clarification on what I am talking about
-
-
+Body of main.py
 '''
+if not run_loop:
 
-# Setting up a range for testing beta values
-beta_testing = {}
+    # Getting MAP and MLE values for beta = v_total 
+    lh = build_MAP_MLE([[0 for x in range(v_total+1)] for y in range(unique_targets+1)], beta=v_total) 
 
-beta_range = np.linspace(100000,1,50)
-print(beta_range)
-b_length = len(beta_range)
+    # Turning lh into an array and then saving it to a file (np ~ numpy)
+    lh_np = np.asarray(lh)
+    lh_np = np.delete(lh_np, 0, 0) # delete row 0, all zero's
+    savetxt('Project_2/lh_np.csv', lh_np, delimiter=',')
 
-for i in range(b_length):
-    print(round(beta_range[i]))
-    current_beta = build_MAP_MLE([[0 for x in range(v_total+1)] for y in range(unique_targets+1)], beta=round(beta_range[i])) 
-    current_beta = np.asarray(current_beta)
-    current_beta = np.delete(current_beta, 0, 0) # delete row 0, all zero's
-    savetxt('Project_2/Diff_Beta_Values/beta_'+str(i)+'.csv', current_beta, delimiter=',')
+else:
+
+    # Setting up a range for testing beta values
+    beta_testing = {}
+    beta_range = np.linspace(100000,1,50) # Beta values from 0.00001 to 1 
+    #(Note: when used in the alpha function beta becomes 1/'assigned value')
+    # print(beta_range)
+    b_length = len(beta_range)
+
+    # Getting MAP and MLE values for each beta value in beta_testing and saving them to a folder 
+    for i in range(b_length):
+        print(round(beta_range[i]))
+        current_beta = build_MAP_MLE([[0 for x in range(v_total+1)] for y in range(unique_targets+1)], beta=round(beta_range[i])) 
+        current_beta = np.asarray(current_beta)
+        current_beta = np.delete(current_beta, 0, 0) # delete row 0, all zero's
+        savetxt('Project_2/Diff_Beta_Values/beta_'+str(i)+'.csv', current_beta, delimiter=',')
 
 
--- Project_2/test.py
+++ Project_2/test.py
@@ -10,15 +10,34 @@ import math
 import matplotlib.pyplot as plt
 from collections import Counter
 
-def get_results(test_np, lh_np, prior):
+'''
+Adjust these global variables for specific training set:
+'''
+run_loop = False # Set to false so that results and accuracy is found for the single beta value
 
-    print()
-    print(lh_np[0][0:4])
+plot_results = False # Set to true to plot results (single beta value will plot true target vs
+# predicted target)
+
+true_targets = True # Set to true if the test data set has the true target values in the last column
+'''
+End of global variables  
+'''
+
+'''
+get_results returns a list of predicted target values as well as a list of the 
+true target values if the test data provides it
+'''
+def get_results(test_np, lh_np, prior, true_targets_exist):
+
+    # print()
+    # print(lh_np[0][0:4])
     result = []
+    y = None
     # print(test_np.shape)
-    y = test_np[:, lh_np.shape[1]] # Creating list from last column (news groups)
-    # Delete last column (news group)
-    test_np = np.delete(test_np, lh_np.shape[1] - 1, 1)
+    if true_targets_exist:
+        y = test_np[:, lh_np.shape[1]] # Creating list from last column (news groups)
+        # Delete last column (news group)
+        test_np = np.delete(test_np, lh_np.shape[1] - 1, 1)
 
     test_np_len = len(test_np)
 
@@ -50,7 +69,10 @@ def get_results(test_np, lh_np, prior):
     # print(result)
     return result, y
 
-
+'''
+get_accuracy returns the number of correctly predicted targets over the total number of predicted 
+targets to get the precent correct
+'''
 def get_accuracy(test_np, result, y):
     test_np_len = len(test_np)
     accuracy_count = 0
@@ -63,61 +85,71 @@ def get_accuracy(test_np, result, y):
     return accuracy_count / test_np_len
 
 
+'''
+Body of test.py
+'''
 # Loading MAP and MLE values from latest training run
 lh_np = loadtxt('Project_2/lh_np.csv', delimiter=',')
-print(lh_np.shape)
-
-prior = lh_np[:, lh_np.shape[1] - 1] # List of priors for each news group
-# List of MAP values for each news group
-lh_np = np.delete(lh_np, lh_np.shape[1] - 1, 1)
 # print(lh_np.shape)
 
+prior = lh_np[:, lh_np.shape[1] - 1] # List of priors for each news group
+lh_np = np.delete(lh_np, lh_np.shape[1] - 1, 1) # List of MAP values for each news group
+# print(lh_np.shape)
+
+# Loading the testing data set 
 test_sparse = sparse.load_npz('Project_2/csr_test.csv_lg.npz')
 test = sparse.csr_matrix(test_sparse)
 test_np = csr_matrix.toarray(test)
 test_np = np.delete(test_np, 0, 1)  # Delete 1st column (index)
 
-result,y = get_results(test_np, lh_np, prior)
-accuacy = get_accuracy(test_np, result, y)
+if not run_loop:
+    # Getting predictions for the test data 
+    result,y = get_results(test_np, lh_np, prior, true_targets)
+    savetxt('Project_2/NB_result.csv', result, delimiter=',', header='predictions', 
+           fmt ='% s')
+    if true_targets:
+        # Getting the accuracy of the test data set's predictions
+        accuracy = get_accuracy(test_np, result, y)
+     
+        if plot_results:
 
+            # Creating scatter plot of results 
+            c = Counter(zip(result.copy(),y.copy()))
+            # Creating list to increase size of points with greater frequency 
+            s = [10*c[(results,yy)] for results,yy in zip(result,y)] 
+            # Creating actual plot and showing it
+            plt.scatter(result, y, s=s)
+            plt.locator_params(axis="both", integer=True, tight=True)
+            plt.ylim(0,lh_np.shape[0] + 1)
+            plt.xlim(0,result.shape[0] + 1)
+            plt.show()
 
-# # Creating scatter plot of results 
-# # Y-axis: True values representing news groups
-# # X-axis: Predicted values representing news groups
-# c = Counter(zip(result.copy(),y.copy()))
-# # Creating list to increase size of points with greater frequency 
-# s = [10*c[(results,yy)] for results,yy in zip(result,y)] 
-# # Creating actual plot and showing it
-# plt.scatter(result, y, s=s)
-# plt.locator_params(axis="both", integer=True, tight=True)
-# plt.ylim(0,lh_np.shape[0] + 1)
-# plt.xlim(0,result.shape[0] + 1)
-# plt.show()
+else: 
+    beta_test_results = []
 
+    for i in range(0,50):
+        beta_MAP_MLE = loadtxt('Project_2/Diff_Beta_Values/beta_'+str(i)+'.csv', delimiter=',')
+        beta_prior = beta_MAP_MLE[:, beta_MAP_MLE.shape[1] - 1] # List of priors for each news group
+        # List of MAP values for each news group
+        beta_MAP_MLE = np.delete(beta_MAP_MLE, beta_MAP_MLE.shape[1] - 1, 1)
 
-beta_test_results = []
+        # print()
+        # print(beta_MAP_MLE[0][0:4])
 
-for i in range(0,50):
-    beta_MAP_MLE = loadtxt('Project_2/Diff_Beta_Values/beta_'+str(i)+'.csv', delimiter=',')
-    beta_prior = beta_MAP_MLE[:, beta_MAP_MLE.shape[1] - 1] # List of priors for each news group
-    # List of MAP values for each news group
-    beta_MAP_MLE = np.delete(beta_MAP_MLE, beta_MAP_MLE.shape[1] - 1, 1)
+        beta_result,beta_y = get_results(test_np, beta_MAP_MLE, beta_prior, true_targets)
 
-    # print()
-    # print(beta_MAP_MLE[0][0:4])
+        if true_targets:
+            beta_accuracy = get_accuracy(test_np, beta_result, beta_y)
 
-    beta_result,beta_y = get_results(test_np, beta_MAP_MLE, beta_prior)
-    beta_accuracy = get_accuracy(test_np, beta_result, beta_y)
+            beta_test_results.append(beta_accuracy)
 
-    beta_test_results.append(beta_accuracy)
-
-# print(beta_test_results)
-x = np.linspace(0.00001,1,50)
-# print(x)
-
-plt.plot(x,beta_test_results)
-plt.xscale("log")
-plt.show()
+    if plot_results and true_targets:
+        # print(beta_test_results)
+        x = np.linspace(0.00001,1,50)
+        # print(x)
+        plt.plot(x,beta_test_results)
+        plt.xscale("log")
+        plt.show()
 
 
 
