3299c8023fce3f1a1094bc851b5b30bbda47ed9d
Michael Servilla <miservilla@gmail.com>
2023-04-06 22:49:50 -0600

    Current working files TF_vision.py and vit.py

--- Project_3/make_image_dictionary.py
+++ Project_3/make_image_dictionary.py
@@ -1,6 +1,7 @@
 import os
 import pyarrow as pa
 import pyarrow.dataset as ds
+import pyarrow.feather as ft
 import pandas as pd
 import numpy as np
 from datasets import Dataset
@@ -9,8 +10,8 @@ import cv2
 from PIL import Image
 import datasets
 
-rootdir = '/home/michaelservilla/CS529/Project_3/train'
-train_small = []
+rootdir = '/home/michaelservilla/CS529/Project_3/train (copy)'
+train = []
 bad = []
 count = 0
 feature = datasets.Image(decode=False)
@@ -23,7 +24,7 @@ for subdir, dirs, files in os.walk(rootdir):
         dir = subdir.split('/')[-1]
         img = Image.open(path)
         # img = img.resize((32, 32))
-        img.save(path)
+        # img.save(path)
         w, h = img.size
         if w != h:
             print(path)
@@ -33,7 +34,7 @@ for subdir, dirs, files in os.walk(rootdir):
         # img = cv2.imread(path)
         # cv2.imshow('OutputImage', img)
         # cv2.waitKey(0)
-        train_small.append([new_img,dir])
+        train.append([new_img,dir])
         print(count, w, h)
 
 # print(train_small)
@@ -41,18 +42,21 @@ for subdir, dirs, files in os.walk(rootdir):
 # for i, j in train.items():
 #     print(i, j)
 
-train_small = pd.DataFrame(train_small, columns=['img', 'label'])
-print(train_small)
+train = pd.DataFrame(train, columns=['img', 'label'])
+print(train)
+train.to_pickle("./Project_3/plants_df")
 
 # plants = ds.dataset(pa.Table.from_pandas(train))
 
 # plants.save_to_disk('plants')
 # print(plants)
 
-# hg_plants_small = ds.dataset(pa.Table.from_pandas(train_small).to_batches())
-# # print(type(hg_plants_small))
+# hg_plants = ds.dataset(pa.Table.from_pandas(train).to_batches())
+# print(type(hg_plants))
 
-# hg_plants_small.save_to_disk('plants_small')
+# ds.write_dataset(hg_plants, './Project_3/plants')
+
+# hg_plants.save_to_disk('plants')
 
 # df_train, df_test = train_test_split(train, test_size=0.2)
 
